acf0db601841519281f786cc30866c38b38046a0
Igor Alyanov <ii.ielyanov@gmail.com>
2018-05-04 23:36:22 +0300

    Create Admin Panel

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,17 @@
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the 'web' middleware group. Now create something great!
 |
 */
 
+Route::group(['prefix'=>'admin', 'namespace'=>'Admin', 'middlewire'=>['auth']], function(){
+    Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+});
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
